e65e9bb70d83bb75b3da8f478b93c8cbbeed72c2
umer2001 <umer2001.uf@gmail.com>
2021-07-23 19:27:09 +0500

    change : app to show form & loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import { Button, Typography } from "antd";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./Form";
+import Loading from "./Loading";
 
-function App() {
+const App: FC = () => {
+  const { Title, Text } = Typography;
   return (
     <div className="App">
+      {/* TODO: change Header tag */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Quizzie!!
-        </p>
+        <Title level={2}>Quizzie!!</Title>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          autem eveniet rem, facilis deleniti eum consequuntur molestiae.
+          Perspiciatis aut nihil inventore porro aperiam, iure ea ratione non
+          harum ab ullam!
+        </Text>
+        <Button type="primary" size="large">
+          Start
+        </Button>
+        {/* TODO: if need loading */}
+        <Loading />
+        <Form />
       </header>
     </div>
   );
-}
+};
 
 export default App;
