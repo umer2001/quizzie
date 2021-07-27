18029baa53357478b6214dcc65920427c3eee2b8
umer2001 <umer2001.uf@gmail.com>
2021-07-27 10:36:59 +0500

    change : some styles

--- src/StatusBox.tsx
+++ src/StatusBox.tsx
@@ -1,16 +1,15 @@
 import React, { FC } from "react";
 import { CloseSquareFilled, CheckSquareFilled } from "@ant-design/icons";
 import Square from "./Square";
+import { AnswerStatus } from "./models";
 
-const StatusBox: FC<{ status: "right" | "wrong" | "unknown" }> = ({
-  status,
-}) => {
+const StatusBox: FC<{ status: AnswerStatus }> = ({ status }) => {
   return status === "unknown" ? (
     <Square />
   ) : status === "wrong" ? (
-    <CloseSquareFilled style={{ color: "red" }} />
+    <CloseSquareFilled style={{ fontSize: "33px", color: "#ff4d4f" }} />
   ) : (
-    <CheckSquareFilled style={{ color: "#52c41a" }} />
+    <CheckSquareFilled style={{ fontSize: "33px", color: "#52c41a" }} />
   );
 };
 
